7f937adfadca8cdfae5e13bd7b30ef3988a60b65
karla3l <kah_s0uza@hotmail.com>
2023-08-15 08:06:23 -0400

    resolution of the first and second list of activities

---
+++ 11Aula/1aula.py
@@ -0,0 +1,28 @@
+print ("Olá, mundo!")
+#----------------------------------------------------------------
+x = 10
+print (x)
+#----------------------------------------------------------------
+x = 10 
+print (x*3/2-x)
+#----------------------------------------------------------------
+x = 5
+y = 10
+print("A soma de", x, "e", y, "é", x + y)
+#----------------------------------------------------------------
+nome = input("Qual é o seu nome? \t")
+print ("Olá", nome, "!")
+#----------------------------------------------------------------
+n1 = int(input('Digite um número: \t'))
+n2 = int(input('Digite outro número: \t'))
+soma = n1 + n2
+print ('A soma dos números digitados é', soma)
+#----------------------------------------------------------------
+numero1 = 3.4
+numero2 = "2.5"
+numero3 = float(numero2)
+numero4 = float(input('Digite um numero decimal: \t'))
+acerto = numero1 + numero3 + numero4
+print(acerto)
+#erro = numero1 + numero2 não vai rodar pq são tipos diferentes
+#----------------------------------------------------------------
---
+++ 12Aula/2aula.py
@@ -0,0 +1,8 @@
+# RESTO DA DIVISÃO
+dividendo = 17
+divisor = 5
+cociente = dividendo // divisor
+resto = dividendo % divisor
+print("dividendo = divisor 8 cociente + resto")
+print(dividendo, '=', divisor, '*', cociente, '+', resto)
+###############################################################
